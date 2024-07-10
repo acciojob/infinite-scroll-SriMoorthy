6cc3685c0e321d526411e8c783f32c97cbdf4863
SriMoorthy <srimoorthy27@gmail.com>
2024-07-10 16:00:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here!
+$input = document.getElementById('infi-list')
+
+for (let i = 0; i < 11; i++) {
+	$list = document.createElement('li')
+	$list.innerText = `Item ${i}`
+	$input.appendChild($list)
+}
 
