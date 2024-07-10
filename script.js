f2bee13619ea0d14a3bc1070f07b9b40094c0a8c
SriMoorthy <srimoorthy27@gmail.com>
2024-07-10 16:02:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your code here!
 $input = document.getElementById('infi-list')
 
-for (let i = 0; i < 11; i++) {
+for (let i = 0; i < 1000; i++) {
 	$list = document.createElement('li')
 	$list.innerText = `Item ${i}`
 	$input.appendChild($list)
